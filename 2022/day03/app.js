4e202de3ccb099fc86ce1c820cd39eb32140cd50
Siddheshwar Vikram Ghumare <siddheshwar.ghumare@ptw.com>
2023-01-03 12:31:31 +0530

    Day 03 Part 2 - Done.

--- 2022/day03/app.js
+++ 2022/day03/app.js
@@ -23,4 +23,34 @@ fileContent.split("\n").map((string) => {
   const matchedNumber = getMatchedCharacterFromString(string);
   sum += matchedNumber;
 });
-console.log("Sum", sum);
+console.log("Part 1 Sum", sum);
+
+function sliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
+
+function getMatchedCharacterFromArray(groupArray) {
+  groupArray.sort((a, b) => b.length - a.length);
+  const firstString = groupArray[0] || "";
+  const secondString = groupArray[1] || "";
+  const thirdString = groupArray[2] || "";
+  for (let i = 0; i < firstString.length; i += 1) {
+    if (
+      secondString.indexOf(firstString[i]) !== -1 &&
+      thirdString.indexOf(firstString[i]) !== -1
+    ) {
+      return getNumberFromAlphabet(firstString[i]);
+    }
+  }
+}
+let partTwoSum = 0;
+sliceIntoChunks(fileContent.split("\n"), 3).map((groupArray) => {
+  const matchedNumber = getMatchedCharacterFromArray(groupArray);
+  partTwoSum += matchedNumber;
+});
+console.log("Part 2 Sum", partTwoSum);
