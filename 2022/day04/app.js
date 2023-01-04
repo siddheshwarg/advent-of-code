b3de2df8613c2a4763ee5de61932ba48a54af78d
Siddheshwar Vikram Ghumare <siddheshwar.ghumare@ptw.com>
2023-01-04 10:50:47 +0530

    Day 04 Part 2 - Done.

--- 2022/day04/app.js
+++ 2022/day04/app.js
@@ -32,10 +32,35 @@ function isFullyContainsOther(eachPair) {
   }
 }
 
+function isFullyContainsTwoPairByOverlap(firstPair, secondPair) {
+  const firstStart = parseInt(firstPair.split("-")[0]);
+  const firstEnd = parseInt(firstPair.split("-")[1]);
+  const secondStart = parseInt(secondPair.split("-")[0]);
+  const secondEnd = parseInt(secondPair.split("-")[1]);
+  const firstCondition = firstStart <= secondStart && firstEnd >= secondStart;
+  const secondCondition = firstStart <= secondEnd && firstEnd >= secondEnd;
+  const thirdCondition = secondStart <= firstStart && secondEnd >= firstStart;
+  const fourthCondition = secondStart <= firstEnd && secondEnd >= firstEnd;
+  return firstCondition || secondCondition || thirdCondition || fourthCondition;
+}
+
+function isFullyContainsOtherByOverlap(eachPair) {
+  const firstPair = eachPair.split(",")[0];
+  const secondPair = eachPair.split(",")[1];
+  if (isFullyContainsTwoPairByOverlap(firstPair, secondPair)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 let fullyContainsOtherCount = 0;
+let overlapCount = 0;
 fileContent
   .split(fileContent.includes("\r\n") ? "\r\n" : "\n")
   .map((eachPair) => {
     fullyContainsOtherCount += isFullyContainsOther(eachPair);
+    overlapCount += isFullyContainsOtherByOverlap(eachPair);
   });
 console.log("Part 1 count " + fullyContainsOtherCount);
+console.log("Part 2 count " + overlapCount);
