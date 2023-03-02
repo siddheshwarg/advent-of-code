1b56b42bde250b5221f585bcdf82bdac66a6c46d
Siddheshwar Vikram Ghumare <siddheshwar.ghumare@ptw.com>
2023-03-02 20:09:54 +0530

    Day 06 Part 2 - Done.

--- 2022/day06/app.js
+++ 2022/day06/app.js
@@ -3,20 +3,33 @@ import loadInputFromTextFile from "../../utils/index.js";
 // const fileContent = loadInputFromTextFile(process.cwd() + "/demo-input.txt");
 const fileContent = loadInputFromTextFile(process.cwd() + "/input.txt");
 
-const characterCount = 4;
+const duplicateCharactersOrNot = (string) => {
+  return string.split("").some(function (v, i, a) {
+    return a.lastIndexOf(v) != i;
+  });
+};
 
-let firstMarkerAfterCharacter = 0;
-for (let itr = 0; itr < fileContent.length; itr++) {
-  const duplicatesOrNot = fileContent
-    .substring(itr, characterCount + itr)
-    .split("")
-    .some(function (v, i, a) {
-      return a.lastIndexOf(v) != i;
-    });
-  if (!duplicatesOrNot) {
-    firstMarkerAfterCharacter = itr + characterCount;
-    break;
+const getFirstMarkerAfterCharacter = (characterSetCount) => {
+  let firstMarkerAfterCharacter = 0;
+  for (let charIndex = 0; charIndex < fileContent.length; charIndex++) {
+    if (
+      !duplicateCharactersOrNot(
+        fileContent.substring(charIndex, characterSetCount + charIndex)
+      )
+    ) {
+      firstMarkerAfterCharacter = charIndex + characterSetCount;
+      break;
+    }
   }
-}
+  return firstMarkerAfterCharacter;
+};
 
-console.log("first marker after character ", firstMarkerAfterCharacter);
+console.log(
+  "Part One - first marker after character ",
+  getFirstMarkerAfterCharacter(4)
+);
+
+console.log(
+  "Part Two - first marker after character ",
+  getFirstMarkerAfterCharacter(14)
+);
